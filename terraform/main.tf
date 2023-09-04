72621d91e3267c07f937b4e5ece4e78ede417f77
Evgeniy Korolev <bo0oms0nic@mail.ru>
2023-09-04 18:28:07 +0300

    Added lesson 2 homework

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+data "yandex_compute_image" "get_ubuntu_image" {
+  family = var.get_ubuntu_image
+}
+
+resource "yandex_compute_instance" "vm" {
+  # count = var.vms_count
+  # name  = "${var.project}-vm-${count.index}"
+  name  = "${var.project}-vm"
+
+  resources {
+    cores  = var.cpu_count
+    memory = var.ram_count
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "${data.yandex_compute_image.get_ubuntu_image.id}"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+  }
+  # metadata = {
+  #   user-data = "${file("files/default-users.txt")}"
+  # }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = yandex_compute_instance.vm.network_interface.0.nat_ip_address
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Im ready!'"]
+
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -u ubuntu -i '${self.network_interface.0.nat_ip_address},' --private-key ~/.ssh/id_rsa provision.yml"
+  }
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "${var.project}-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "${var.project}-subnet"
+  zone           = var.zone
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = var.ipv4_cidr_blocks
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+#auth_yandex
+variable "service_account_key_file" {
+  type        = string
+  description = "Key .json"
+}
+variable "cloud_id" {
+  type        = string
+  description = "Default cloud-id"
+}
+variable "folder_id" {
+  type        = string
+  description = "Default folder-id"
+}
+variable "zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Default zone"
+}
+variable "project" {
+  type = string
+  default = "otus-c0r2jh"
+}
+
+#vm_settings
+variable "vms_count" {
+  type        = string
+  default     = "1"
+  description = "VMs count"
+}
+variable "cpu_count" {
+  type        = string
+  default     = "2"
+  description = "Numbers of CPU, count"
+}
+variable "ram_count" {
+  type        = string
+  default     = "8"
+  description = "RAM, GB"
+}
+variable "get_ubuntu_image" {
+  type        = string
+  default     = "ubuntu-1804-lts"
+  description = "Default Ubuntu OS image id"
+}
+variable "ipv4_cidr_blocks" {
+  type = list
+  default = ["192.168.10.0/24"]
+}
+
